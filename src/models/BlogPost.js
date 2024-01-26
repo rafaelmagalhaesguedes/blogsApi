bd950865b8bb11d0ad9ff1fbb16c00e5188d7119
rafael <rafaelmagalhaes1@live.com>
2024-01-26 20:38:01 -0300

    fix: BlogPost Model fixed

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,13 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogSpotModels = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
-    published: DataTypes.STRING
+    userId: DataTypes.INTEGER,
+    published: DataTypes.STRING,
+    updated: DataTypes.STRING,
   }, {
     tableName: 'blog_posts',
     timestamps: false,
     underscored: true,
   });
+
+  BlogSpotModels.associate = (models) => {
+    BlogSpotModels.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  }
 
   return BlogSpotModels;
 };
