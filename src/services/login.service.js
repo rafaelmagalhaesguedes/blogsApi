d0a4e03c64d899322de16585466ba2ceb620aeb7
rafael <rafaelmagalhaes1@live.com>
2024-01-27 10:47:05 -0300

    feat: create login.service on services layer

---
+++ src/services/login.service.js
@@ -0,0 +1,46 @@
+const { createToken } = require('../utils/auth');
+const { bodySchema } = require('./validations/login.schema');
+const { User } = require('../models');
+
+/* 
+  Validate the request body
+*/
+const validateRequestBody = (email, password) => {
+  const { error } = bodySchema.validate({ email, password });
+  if (error) throw new Error('Some required fields are missing');
+};
+
+/*
+  Find the user by email
+*/
+const findUser = async (email) => {
+  const user = await User.findOne({ where: { email } });
+  if (!user) throw new Error('Invalid fields');
+  return user;
+};
+
+/* 
+  Validate the password
+*/
+const validatePassword = (password, userPassword) => {
+  if (password !== userPassword) throw new Error('Invalid fields');
+};
+
+/* 
+  Login service
+*/
+const loginService = async (email, password) => {
+  validateRequestBody(email, password);
+
+  const user = await findUser(email);
+
+  validatePassword(password, user.password);
+
+  const token = createToken({ id: user.userId, email: user.userEmail });
+
+  return { status: 'SUCCESSFUL', data: { token } };
+};
+
+module.exports = { 
+  loginService,
+};
