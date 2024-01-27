d989b6c89f6b12a6fde98d3705103f7b16762963
rafael <rafaelmagalhaes1@live.com>
2024-01-27 19:16:38 -0300

    feat: add route to get user by id GET /users/:id

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,8 +4,13 @@ const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
 
+// Route to create a new user
 router.post('/', userController.createUser);
 
+// Route to get all users
 router.get('/', authenticate, userController.getAllUsers);
+
+// Route to get user by id
+router.get('/:id', authenticate, userController.getUserById);
 
 module.exports = router;
