ba1d3943763d2189583cd05e0d47e2bc35a0440c
rafael <rafaelmagalhaes1@live.com>
2024-01-27 09:35:26 -0300

    refact: remove sequelize authenticate

--- src/server.js
+++ src/server.js
@@ -1,14 +1,7 @@
 const app = require('./app');
-const sequelize = require('./models');
 
 const port = process.env.API_PORT || 3001;
 
-app.listen(port, async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    console.log(`App listening on port ${port}`);
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
