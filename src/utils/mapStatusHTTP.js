a3325f7a3cdd2377614372c25da91ecb49c1beb1
rafael <rafaelmagalhaes1@live.com>
2024-01-27 10:46:07 -0300

    fix: file statusHTTPmap

--- src/utils/mapStatusHTTP.js
+++ src/utils/mapStatusHTTP.js
@@ -1,14 +1,11 @@
-const httpErrorMap = {
+const statusHTTP = {
   SUCCESSFUL: 200,
   CREATED: 201,
   NOT_FOUND: 404,
   CONFLICT: 409,
-  INVALID_VALUE: 422,
+  INVALID_VALUE: 400,
   UNAUTHORIZED: 401,
   INTERNAL_ERROR: 500,
-  INVALID_FIELDS: 400,
 };
 
-const mapStatusHTTP = (status) => httpErrorMap[status] || 500;
-
-module.exports = mapStatusHTTP;
+module.exports = statusHTTP;
