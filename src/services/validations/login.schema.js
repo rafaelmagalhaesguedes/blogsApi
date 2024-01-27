e12e4ee0bb6677f020acc5235b126f1e7c4d660a
rafael <rafaelmagalhaes1@live.com>
2024-01-27 10:47:43 -0300

    feat: create login.schema to validate inputs

---
+++ src/services/validations/login.schema.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+const errorMessage = 'Some required fields are missing';
+
+const bodySchema = Joi.object({
+  email: Joi.string().required().messages({
+    'string.required': errorMessage,
+    'string.empty': errorMessage,
+  }),
+  password: Joi.string().required().messages({
+    'string.required': errorMessage,
+    'string.empty': errorMessage,
+  }),
+});
+
+module.exports = { 
+  bodySchema,
+};
