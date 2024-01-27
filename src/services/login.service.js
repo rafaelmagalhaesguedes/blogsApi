6c3d3a12becf0efde9516a0ff36271233ad10d50
rafael <rafaelmagalhaes1@live.com>
2024-01-27 19:04:24 -0300

    refact: loginService on services layer

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,41 +1,17 @@
 const { createToken } = require('../utils/auth');
-const { bodySchema } = require('./validations/schemas/login.schema');
-const { User } = require('../models');
+const login = require('./validations/login.validate');
 
-/* 
-  Validate the request body
-*/
-const validateRequestBody = (email, password) => {
-  const { error } = bodySchema.validate({ email, password });
-  if (error) throw new Error('Some required fields are missing');
-};
-
-/*
-  Find the user by email
-*/
-const findUserByEmail = async (email) => {
-  const user = await User.findOne({ where: { email } });
-  if (!user) throw new Error('Invalid fields');
-  return user;
-};
-
-/* 
-  Validate the password
-*/
-const validatePassword = (password, userPassword) => {
-  if (password !== userPassword) throw new Error('Invalid fields');
-};
-
-/* 
-  Login service
-*/
 const loginService = async (email, password) => {
-  validateRequestBody(email, password);
+  // Validate the request body
+  login.validateRequestBody(email, password);
 
-  const user = await findUserByEmail(email);
+  // Find the user by email
+  const user = await login.validateUserByEmail(email);
 
-  validatePassword(password, user.password);
+  // Validate the password
+  login.validatePassword(password, user.password);
 
+  // Create the token
   const token = createToken({ id: user.userId, email: user.userEmail });
 
   return { status: 'SUCCESSFUL', data: { token } };
