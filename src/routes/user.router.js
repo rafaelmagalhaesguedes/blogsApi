ba7b668b2645d6ddbd9cae58ce0c72146fe9f1a2
rafael <rafaelmagalhaes1@live.com>
2024-01-27 18:50:20 -0300

    feat: create route GET /users and findAllUsers

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { userController } = require('../controllers');
+const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/', userController.createUser);
+
+router.get('/', authenticate, userController.getAllUsers);
 
 module.exports = router;
