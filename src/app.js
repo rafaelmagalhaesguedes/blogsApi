f2fda68ce956cdced65bc75a3f775509d6d8248d
rafael <rafaelmagalhaes1@live.com>
2024-01-27 09:32:15 -0300

    feat: create routes dir and login routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 const express = require('express');
-
-// ...
+const routes = require('./routes/login.routes');
 
 const app = express();
 
-// não remova ou mova esse endpoint
-app.get('/', (_request, response) => {
-  response.send();
-});
+app.get('/', (_request, response) => response.send());
 
 app.use(express.json());
+app.use(routes);
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
