4d1a7ef3bd8deb7563c9573d9ad403987ace8f5b
rafael <rafaelmagalhaes1@live.com>
2024-01-28 22:36:02 -0300

    feat: add createPost function on controllers layer

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,18 @@
+const { postService } = require('../services');
+const httpStatus = require('../utils/mapStatusHTTP');
+
+const createPost = async (req, res) => {
+  const post = req.body;
+  const userId = req.user.id;
+
+  try {
+    const { status, data } = await postService.createPost(post, userId);
+    res.status(httpStatus[status]).json(data);
+  } catch (error) {
+    return res.status(httpStatus.INVALID_VALUE).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createPost,
+};
