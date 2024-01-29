399feb6e61bc54fbe1376b71b7041e468a9a05cb
rafael <rafaelmagalhaes1@live.com>
2024-01-29 09:44:09 -0300

    feat: getAllPost function created on controllers layer

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -13,6 +13,16 @@ const createPost = async (req, res) => {
   }
 };
 
+const getAllPosts = async (req, res) => {
+  try {
+    const { status, data } = await postService.getAllPosts();
+    res.status(httpStatus[status]).json(data);
+  } catch (error) {
+    return res.status(httpStatus.INVALID_VALUE).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createPost,
+  getAllPosts,
 };
