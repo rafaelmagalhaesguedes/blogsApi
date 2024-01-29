a5d923a84e212de9a02cbddb3cb1df087ca4ac1d
rafael <rafaelmagalhaes1@live.com>
2024-01-28 22:33:05 -0300

    refact: user.service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,37 +2,27 @@ const { User } = require('../models');
 const { createToken } = require('../utils/auth');
 const { validateUserBody, validateUserByEmail } = require('./validations/user.validate');
 
-/* 
-  Create new user
-*/
 const createUser = async (displayName, email, password, image) => {
-  // Validate user body data 
   validateUserBody(displayName, email, password, image);
 
-  // Validate if user already exists
   await validateUserByEmail(email);
 
-  // Create user and token
   const user = await User.create({ displayName, email, password, image });
-  const token = createToken({ email: user.userEmail });
+  const token = createToken({ id: user.id });
 
   return { status: 'CREATED', data: { token } };
 };
 
-/* 
-  Get all users
-*/
 const getAllUsers = async () => {
   const users = await User.findAll({ attributes: { exclude: ['password'] } });
+
   return { status: 'SUCCESSFUL', data: users };
 };
 
-/* 
-  Get user by id
-*/
 const getUserById = async (id) => {
   const user = await User.findByPk(id, { attributes: { exclude: ['password'] } });
   if (!user) throw new Error('User does not exist');
+
   return { status: 'SUCCESSFUL', data: user };
 };
 
