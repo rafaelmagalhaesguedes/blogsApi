bf04536e8508dce555fd966776550dc949d01c07
rafael <rafaelmagalhaes1@live.com>
2024-01-29 14:00:29 -0300

    feat: createPostCategory created on category.service layer

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,23 +1,28 @@
-const { Category } = require('../models');
+const { Op } = require('sequelize');
+const { Category, PostCategory } = require('../models');
 
 const createCategory = async (name) => {
-  // Validate if name is empty
   if (!name) return { status: 'INVALID_VALUE', data: { message: '"name" is required' } };
-  
-  // Create category
   const category = await Category.create({ name });
 
   return { status: 'CREATED', data: category };
 };
 
-const getAllCategories = async () => {
-  // Get all categories
-  const categories = await Category.findAll();
+const createPostCategory = async (categoryIds, postId) => {
+  const categories = await Category.findAll({ where: { id: { [Op.in]: categoryIds } } });
+  const newPostCategories = categories.map((categorie) => (
+    { postId, categoryId: categorie.id }
+  ));
+  await PostCategory.bulkCreate(newPostCategories);
+};
 
+const getAllCategories = async () => {
+  const categories = await Category.findAll();
   return { status: 'SUCCESSFUL', data: categories };
 };
 
 module.exports = {
   createCategory,
+  createPostCategory,
   getAllCategories,
 };
