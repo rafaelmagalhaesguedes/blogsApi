0cf088c3215c9fec01f39658a4b25814a59610cd
rafael <rafaelmagalhaes1@live.com>
2024-01-28 22:33:26 -0300

    refact: login.service

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,18 +1,18 @@
 const { createToken } = require('../utils/auth');
-const login = require('./validations/login.validate');
+const { loginValidate } = require('./validations');
 
 const loginService = async (email, password) => {
   // Validate the request body
-  login.validateRequestBody(email, password);
+  loginValidate.validateRequestBody(email, password);
 
   // Find the user by email
-  const user = await login.validateUserByEmail(email);
+  const user = await loginValidate.validateUserByEmail(email);
 
   // Validate the password
-  login.validatePassword(password, user.password);
+  loginValidate.validatePassword(password, user.password);
 
   // Create the token
-  const token = createToken({ id: user.userId, email: user.userEmail });
+  const token = createToken({ id: user.id, email: user.email });
 
   return { status: 'SUCCESSFUL', data: { token } };
 };
