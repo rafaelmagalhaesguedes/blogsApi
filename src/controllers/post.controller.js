683d078630915487c1fbed00a429b8c2054a48f9
rafael <rafaelmagalhaes1@live.com>
2024-01-29 22:19:39 -0300

    feat: deletePost function created on controllers layer

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -14,14 +14,12 @@ const createPost = async (req, res) => {
 
 const getAllPosts = async (req, res) => {
   const { status, data } = await postService.getAllPosts();
-
   res.status(httpStatus[status]).json(data);
 };
 
 const getPostById = async (req, res) => {
   const { id } = req.params;
   const { status, data } = await postService.getPostById(id);
-
   res.status(httpStatus[status]).json(data);
 };
 
@@ -37,9 +35,21 @@ const updatePost = async (req, res) => {
   }
 };
 
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+  try {
+    const { status, data } = await postService.deletePost(id, userId);
+    res.status(httpStatus[status]).json(data);
+  } catch (error) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createPost,
   getAllPosts,
   getPostById,
   updatePost,
+  deletePost,
 };
