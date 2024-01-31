e6252cc158f5c241432458eb8ed8159c22b34939
rafael <rafaelmagalhaes1@live.com>
2024-01-31 07:53:25 -0300

    feat: user.repository created

---
+++ src/services/repository/index.js
@@ -0,0 +1,9 @@
+//
+// Barrel Repository
+const postRepository = require('./post.repository');
+const userRepository = require('./user.repository');
+
+module.exports = {
+  postRepository,
+  userRepository,
+};
