fa6f98a8129f47db62617e20dd7357bd9b51f3f9
rafael <rafaelmagalhaes1@live.com>
2024-03-14 21:09:30 -0300

    refact: authMiddleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,6 +2,7 @@ const { splitToken, verifyToken } = require('../utils/auth');
 const { httpStatus } = require('../utils/httpStatus');
 
 const authenticate = (req, res, next) => {
+  //
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -11,12 +12,12 @@ const authenticate = (req, res, next) => {
   try {
     const token = splitToken(authorization);
     const user = verifyToken(token);
-
     req.user = user;
-    next();
   } catch (error) {
     return res.status(httpStatus.UNAUTHORIZED).json({ message: 'Expired or invalid token' });
   }
+
+  next();
 };
 
 module.exports = { 
